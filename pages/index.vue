d95b5596fe42bdb02623503deed58e0daf483d25
Vitalii Dudnik <dudnikvtl@gmail.com>
2024-09-01 20:23:44 +0300

    Add products and details page, create custom error page

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius nam
+      nisi? Quae sint sed cum sapiente suscipit velit aliquam incidunt aut eos.
+      Amet ex voluptas maxime non reiciendis libero.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste eius nam
+      nisi? Quae sint sed cum sapiente suscipit velit aliquam incidunt aut eos.
+      Amet ex voluptas maxime non reiciendis libero.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
